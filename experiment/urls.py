84be50d3cd9c353ac660e65dd23f7231d913ed33
Mawwlle <dunaev.maksim.03@bk.ru>
2024-05-17 23:16:00 +0300

    fix: templates

--- experiment/urls.py
+++ experiment/urls.py
@@ -4,6 +4,7 @@ from experiment import views
 
 app_name = "experiments"
 urlpatterns = [
+    path("", views.index_view, name="index"),
     path("experiments/", views.ExperimentListView.as_view(), name="list"),
     path("experiments/<int:pk>/", views.ExperimentDetailView.as_view(), name="detail"),
     path("runs/<int:pk>/", views.RunDetailView.as_view(), name="run-detail"),
--- experiment/views.py
+++ experiment/views.py
@@ -4,7 +4,11 @@ from django.http import HttpRequest, HttpResponse
 from django.views import generic
 
 from experiment.models import ExperimentModel, RunModel
+from django.shortcuts import render
+from django.db.models import Count
 
+from system.models import CPUStatsModel, MemoryStatsModel, NetworkStatsModel, SystemModel
+from django.db.models import Avg, Sum
 
 class ExperimentListView(generic.ListView):
     template_name = "experiment/list.html"
@@ -34,3 +38,46 @@ class RunDetailView(generic.DetailView):
     model = RunModel
     template_name = "run/detail.html"
     context_object_name = "run"
+
+
+def index_view(request) -> HttpResponse:
+    # Number of unique systems
+    unique_systems_count = SystemModel.objects.values('cpu_name', 'gpu_name', 'ram').distinct().count()
+
+    # Most popular system
+    most_popular_system = (
+        SystemModel.objects.values('cpu_name', 'gpu_name', 'ram')
+        .annotate(count=Count('id'))
+        .order_by('-count')
+        .first()
+    )
+
+    # Longest run
+    longest_run = RunModel.objects.order_by('-duration').first()
+
+    # Average CPU utilization
+    avg_cpu_utilization = CPUStatsModel.objects.aggregate(avg_utilization=Avg('utilization'))['avg_utilization']
+
+    # System with highest memory usage
+    highest_memory_usage_system = (
+        MemoryStatsModel.objects.order_by('-usage_megabytes')
+        .select_related('system')
+        .first()
+        .system
+    )
+
+    # Total network traffic
+    total_network_traffic = NetworkStatsModel.objects.aggregate(
+        total_receive=Sum('receive_megabytes'),
+        total_transmit=Sum('transmit_megabytes'),
+    )
+
+    context = {
+        'unique_systems_count': unique_systems_count,
+        'most_popular_system': most_popular_system,
+        'longest_run': longest_run,
+        'avg_cpu_utilization': avg_cpu_utilization,
+        'highest_memory_usage_system': highest_memory_usage_system,
+        'total_network_traffic': total_network_traffic,
+    }
+    return render(request, 'index.html', context)
